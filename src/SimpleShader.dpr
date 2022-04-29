02113606e31e04b56b6a84885978699eebeb629b
Olaf Monien <olaf@monien.net>
2022-04-29 18:23:43 +0200

    macOS compatibility

--- src/SimpleShader.dpr
+++ src/SimpleShader.dpr
@@ -5,6 +5,7 @@ program SimpleShader;
 uses
   System.StartUpCopy,
   FMX.Forms,
+  FMX.Types,
   Skia.FMX,
   SimpleShaderMain in 'SimpleShaderMain.pas' {Form14};
 
@@ -12,9 +13,14 @@ uses
 
 begin
   GlobalUseSkia := True;
+
+  //For macOS/iOS
+  GlobalUseMetal := True;
+
   // GPU is priorty everywhere but Windows,
   // this line improves Windows shader performance
   GlobalUseSkiaRasterWhenAvailable := False;
+
   Application.Initialize;
   Application.CreateForm(TForm14, Form14);
   Application.Run;
