c2e2a12b31999158a91f690fca20188379e0d2d4
Jim McKeeth <jim@mckeeth.org>
2022-04-28 20:23:29 -0600

    Added Skia to deployment & quick select listbox

--- src/SimpleShader.dpr
+++ src/SimpleShader.dpr
@@ -1,5 +1,7 @@
 program SimpleShader;
 
+
+
 uses
   System.StartUpCopy,
   FMX.Forms,
--- src/SimpleShaderMain.pas
+++ src/SimpleShaderMain.pas
@@ -6,7 +6,8 @@ uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, StrUtils, Math,
   FMX.Memo.Types, FMX.StdCtrls, FMX.Controls.Presentation, FMX.ScrollBox,
-  FMX.Memo, FMX.TabControl, Skia, Skia.FMX, FMX.Layouts, FMX.MultiView;
+  FMX.Memo, FMX.TabControl, Skia, Skia.FMX, FMX.Layouts, FMX.MultiView,
+  FMX.ListBox, FMX.Ani, FMX.Effects;
 
 type
   TForm14 = class(TForm)
@@ -20,6 +21,13 @@ type
     SkAnimatedPaintBox1: TSkAnimatedPaintBox;
     SpeedButton2: TSpeedButton;
     Splitter1: TSplitter;
+    SpeedButton3: TSpeedButton;
+    lbShaders: TListBox;
+    SkSvg1: TSkSvg;
+    FloatAnimation1: TFloatAnimation;
+    ShadowEffect1: TShadowEffect;
+    Splitter2: TSplitter;
+    ckMouse: TCheckBox;
     procedure Button1Click(Sender: TObject);
     procedure SkAnimatedPaintBox1AnimationDraw(ASender: TObject;
       const ACanvas: ISkCanvas; const ADest: TRectF;
@@ -32,11 +40,16 @@ type
     procedure Splitter1DblClick(Sender: TObject);
     procedure SkAnimatedPaintBox1MouseMove(Sender: TObject;
       Shift: TShiftState; X, Y: Single);
+    procedure SpeedButton3Click(Sender: TObject);
+    procedure lbShadersItemClick(const Sender: TCustomListBox;
+      const Item: TListBoxItem);
   private
     { Private declarations }
     FShader: ISkRuntimeEffect;
     FPaint: ISkPaint;
     procedure RunShader;
+    procedure LoadShaders;
+    procedure LoadShader(const AShaderFile: string);
   public
     { Public declarations }
   end;
@@ -47,6 +60,13 @@ var
 implementation
 
 {$R *.fmx}
+
+uses
+  IOUtils;
+
+const
+  ShaderPath = '..\..\..\Shaders';
+  ShaderExt = '.sksl';
 
 procedure TForm14.RunShader;
 begin
@@ -82,7 +102,7 @@ end;
 procedure TForm14.SkAnimatedPaintBox1MouseMove(Sender: TObject;
   Shift: TShiftState; X, Y: Single);
 begin
-  if FShader.UniformExists('iMouse') then
+  if ckMouse.IsChecked and FShader.UniformExists('iMouse') then
     FShader.SetUniform('iMouse', [X, Y, 0, Math.IfThen(ssLeft in Shift, 1, 0)]);
 end;
 
@@ -91,26 +111,67 @@ begin
   RunShader;
 end;
 
+procedure TForm14.LoadShader(const AShaderFile: string);
+begin
+  Memo1.Lines.LoadFromFile(AShaderFile);
+  Memo1.Lines.Text := Memo1.Lines.Text.Replace(#9, #32);
+  RunShader;
+  MultiView1.HideMaster;
+end;
+
 procedure TForm14.Button2Click(Sender: TObject);
 begin
   if OpenDialog1.Execute then
   begin
-    Memo1.Lines.LoadFromFile(OpenDialog1.FileName);
-    Memo1.Lines.Text := Memo1.Lines.Text.Replace(#9, #32);
-    RunShader;
-    MultiView1.HideMaster;
+    LoadShader(OpenDialog1.FileName);
   end;
 end;
 
 procedure TForm14.FormCreate(Sender: TObject);
 begin
+  LoadShaders;
   MultiView1.Mode := TMultiViewMode.Drawer;
   RunShader;
+end;
+
+procedure TForm14.lbShadersItemClick(const Sender: TCustomListBox;
+  const Item: TListBoxItem);
+begin
+  LoadShader(TPath.Combine(ShaderPath,Item.Text+ShaderExt));
+end;
+
+procedure TForm14.LoadShaders;
+begin
+  var Shaders := TDirectory.GetFiles(ShaderPath,'*'+ShaderExt);
+  for var shader in shaders do
+  begin
+    lbShaders.Items.Add(TPath.GetFileNameWithoutExtension(Shader));
+  end;
+  if lbShaders.Count > 0 then
+  begin
+    lbShaders.ItemIndex := random(lbShaders.Count);
+    LoadShader(TPath.Combine(ShaderPath,lbShaders.Items[lbShaders.ItemIndex]+ShaderExt));
+  end;
 end;
 
 procedure TForm14.SpeedButton2Click(Sender: TObject);
 begin
   MultiView1.HideMaster;
+end;
+
+procedure TForm14.SpeedButton3Click(Sender: TObject);
+begin
+  if BorderStyle = TFmxFormBorderStyle.Sizeable then
+  begin
+    FormStyle := TFormStyle.StayOnTop;
+    BorderStyle := TFmxFormBorderStyle.None;
+    WindowState := TWindowState.wsMaximized;
+  end
+  else
+  begin
+    Close;
+  end;
+
 end;
 
 procedure TForm14.Splitter1DblClick(Sender: TObject);
